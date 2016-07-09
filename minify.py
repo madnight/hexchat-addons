6e0c55c17d50e85b347668860dc7dcaa26de8c55
Fabian Beuke <mail@beuke.org>
2016-07-09 17:54:43 +0200

    improve code

--- minify.py
+++ minify.py
@@ -3,33 +3,24 @@ __module_version__ = "1.0.1"
 __module_description__ = "Parses given text for URLs and shortens them using is.gd"
 __module_author__ = "Lacer <lacer.dragon(at)hotmail.com>, Fabian Beuke <mail(at)beuke.org>"
 
-import hexchat, re
-from pyshorteners.shorteners  import Shortener
+import hexchat, validators
+from pyshorteners.shorteners import Shortener
 
-def minify_callback (word, word_eol, userdata):
-    if len(word) < 2:
-        return hexchat.EAT_ALL
-    
+# Documentation about Python Hexchat Hooks
+# http://hexchat.readthedocs.io/en/latest/script_python.html
+
+def minify_callback(word, word_eol, userdata):
     shorty = Shortener('Isgd')
-    words = word_eol[1].split()
+    words = word_eol[0].split() # word_eol[0] is the full text message
     finalwords = ''
-    for w in words:
-        if is_url(w): # abstracted this out incase I want to replace it
-            w = shorty.short(w) # shrink that bitch
-        finalwords = finalwords + w + ' ' # append each word changed or otherwise back into the sentence
-        
+    for word in words:
+        if validators.url(word):
+            word = shorty.short(word)
+        finalwords += word + ' ' 
     currentchannel = hexchat.get_info('channel')
     hexchat.command("MSG  " + currentchannel + " " + finalwords)
     return hexchat.EAT_ALL
-    
-def is_url(s):
-    # This blob of unholy hatred is from http://stackoverflow.com/questions/6883049/regex-to-find-urls-in-string-in-python
-    url_regex ='http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    if re.search(url_regex, s) == None:
-        return False
-    return True
 
-# Channel hook
-hexchat.hook_print("Your Message", minify_callback)
-# Script load message
-hexchat.prnt("URL Shortener loaded!")
+# empty string hook to capture every message a user sends
+hexchat.hook_command("", minify_callback)
+hexchat.prnt("URL Shortener Version " + __module_version__ + " loaded!")
